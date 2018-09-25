3ded58e0998d7fdb65097110059be7da027a66f5
JonJaryongLee <director9303@gmail.com>
2018-09-25 11:13:30 +0900

    초기구성

---
+++ README.md
@@ -0,0 +1,29 @@
+# my_galaxy
+
+> personal space community
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+##황호룡 프로젝트
+우주 커뮤니티를 만들어보자.
+2018 경상대학교 CS 웹프로그래밍 
+
+황채린
+김호
+이자룡
+
+##9월 23일 작업내용
+기초 세팅중
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div class="start">
+      start
+    </div>
+  </div>
+</template>
+
+<script>
+
+// bootstrap
+  import Vue from 'vue'
+  import BootstrapVue from 'bootstrap-vue'
+  Vue.use(BootstrapVue);
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+
+export default {
+  name: 'app',
+  
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+  .start{
+    display:flex;
+    flex-direction: column;
+    box-sizing:border-box;
+  }
+</style>
