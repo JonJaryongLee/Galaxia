9e1ae536752c1ec0fb2c1e78a05660ad81ee3ee2
JonJaryongLee <director9303@gmail.com>
2018-09-26 16:20:11 +0900

    9월 26일 회의 업로드

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
     <div class="start">
-      start
+      <transition name="welcomeAppear">
+        <div class="welcomePage" v-if="welcomeShow">
+          <h1 class="title">My Galaxy</h1>
+        </div>
+      </transition>
     </div>
   </div>
 </template>
@@ -10,6 +14,7 @@
 
 // bootstrap
   import Vue from 'vue'
+
   import BootstrapVue from 'bootstrap-vue'
   Vue.use(BootstrapVue);
   import 'bootstrap/dist/css/bootstrap.css'
@@ -22,16 +27,52 @@ export default {
   
   data () {
     return {
-      
+      welcomeShow: false
     }
+  },
+  mounted(){
+    this.welcomeShow=true;
   }
 }
 </script>
 
 <style>
-  .start{
+  #app{
     display:flex;
     flex-direction: column;
     box-sizing:border-box;
+  }
+  .start{
+    background-image: url("./assets/image/cute_galaxy.png");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size:cover;
+    background-attachment: fixed;
+    height:789px;
+  }
+  .welcomePage{
+    position: absolute;
+    top:30%;
+    left:38%;
+    width:500px;
+    height:400px;
+    margin:-50px 0 0 -50px;
+    text-align: center;
+  }
+
+  .title{
+    font-size:5.5rem;
+    font-family: 'Space Mono', monospace;
+    color: white;
+  }
+
+  .welcomeAppear-enter{
+    opacity: 0;
+  }
+  .welcomeAppear-enter-active{
+    transition: 2s;
+  }
+  .welcomeAppear-to{
+    opacity: 1;
   }
 </style>
